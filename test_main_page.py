c8ea0492137200eb0a4ac45179328dc5ef87619d
Ruslan Usmanov <rslnusmnv@gmail.com>
2022-05-22 16:10:45 +0400

    - go_to_login_page function added to main_page.py file - test_guest_can_go_to_login_page test added to test_main_page.py files

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,10 @@
+from .pages.main_page import MainPage
+
 LINK = "http://selenium1py.pythonanywhere.com/"
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
-
 def test_guest_can_go_to_login_page(browser):
-    browser.get(LINK)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
